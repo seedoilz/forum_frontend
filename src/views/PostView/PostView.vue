e5b47f0ddbae590c4d4b06c33117d3a2c662ee32
lzyDesktop <lizeyu113@foxmail.com>
2023-12-10 14:22:55 +0800

    postview (without function)

--- src/views/PostView/PostView.vue
+++ src/views/PostView/PostView.vue
@@ -1,22 +1,74 @@
 <template>
-  <div class="post-page">
-    <h1>发布新帖子</h1>
-    <form class="post-form">
-      <div class="form-group">
-        <label for="title">标题</label>
-        <input type="text" id="title" v-model="postMessage.title" required>
-      </div>
-      <div class="form-group">
-        <label for="content">正文</label>
-        <textarea id="content" v-model="postMessage.content" required></textarea>
-      </div>
-      <div class="form-group">
-        <label for="tags">标签</label>
-        <input type="text" id="tags" v-model="postMessage.tags" placeholder="用逗号分隔">
-      </div>
-      <el-button type="primary" plain @click="submitPost">发布</el-button>
-    </form>
-  </div>
+  <el-container class="container-main" >
+    <el-header
+      height="60"
+      class="title">
+      <h1 style="font-size: 3rem;margin-top: 10px;margin-bottom: 10px;color: #DDFF03">☣TITLE</h1>
+    </el-header>
+
+    <el-container>
+    <el-container direction="vertical" class="container-left">
+      <el-header style="text-align: right; font-size: 12px">
+
+      </el-header>
+
+      <el-input
+        placeholder="请输入标题"
+        v-model="input"
+        clearable
+        class="content-title">
+      </el-input>
+
+      <el-form :inline="true" class="middle">
+        <el-form-item class="region">
+          <el-select v-model="region" placeholder="选择板块" class="region-inner" >
+            <el-option label="板块一" value="1"></el-option>
+            <el-option label="板块二" value="2"></el-option>
+          </el-select>
+        </el-form-item>
+
+        <el-tag
+          :key="tag"
+          v-for="tag in dynamicTags"
+          closable
+          :disable-transitions="false"
+          @close="handleClose(tag)">
+          {{tag}}
+        </el-tag>
+
+        <el-input
+          class="input-new-tag"
+          v-if="inputVisible"
+          v-model="inputValue"
+          ref="saveTagInput"
+          size="small"
+          @keyup.enter.native="handleInputConfirm"
+          @blur="handleInputConfirm"
+        >
+        </el-input>
+        <el-button v-else class="button-new-tag" size="small" @click="showInput">+ New Tag</el-button>
+      </el-form>
+
+      <el-input
+        type="textarea"
+        :autosize="{ minRows: 18, maxRows: 22}"
+        placeholder="请输入内容"
+        v-model="textarea"
+        class="content">
+      </el-input>
+    </el-container>
+
+    <el-aside class="aside">
+      <el-input
+        type="textarea"
+        :rows=10
+        placeholder="此处为工具栏等，暂时留空"
+        >
+      </el-input>
+    </el-aside>
+    </el-container>
+
+  </el-container>
 </template>
 
 <script>
@@ -27,6 +79,15 @@ export default {
   name: 'PostView',
   data () {
     return {
+      input: '',
+      region: '',
+
+      dynamicTags: ['标签一', '标签二', '标签三'],
+      inputVisible: false,
+      inputValue: '',
+
+      textarea: '',
+
       postMessage: {
         title: '',
         content: '',
@@ -35,6 +96,25 @@ export default {
     }
   },
   methods: {
+    handleClose (tag) {
+      this.dynamicTags.splice(this.dynamicTags.indexOf(tag), 1)
+    },
+
+    showInput () {
+      this.inputVisible = true
+      this.$nextTick(_ => {
+        this.$refs.saveTagInput.$refs.input.focus()
+      })
+    },
+
+    handleInputConfirm () {
+      let inputValue = this.inputValue
+      if (inputValue) {
+        this.dynamicTags.push(inputValue)
+      }
+      this.inputVisible = false
+      this.inputValue = ''
+    },
     submitPost () {
       console.log(this.postMessage.title)
       if (this.postMessage.title === '') {
@@ -86,67 +166,78 @@ export default {
 </script>
 
 <style>
-body {
-  margin: 0;
-  background-color: #f0f0f0; /* 与post-page相搭配的颜色 */
+.title{
+  border: 1px solid yellow;
+  margin: 10px;
+}
+
+.content-title{
+  border: 1px solid yellow;
+  width: 95%;
+  margin: 10px;
+}
+.content{
+  border: 1px solid yellow;
+  width: 95%;
+  margin: 10px;
+}
+
+.container-main{
+  width: 75%;
+  height: 90vh;
   display: flex;
+  align-content: center;
   justify-content: center;
-  align-items: center;
-  height: 100vh; /* 让内容垂直居中 */
+  margin: auto;
+
 }
 
-.post-page {
-  width: 90vw; /* 宽度为视口宽度的90% */
-  max-width: 800px; /* 设置最大宽度为800px */
-  background-color: #fff;
-  padding: 5vw; /* 以视口宽度的百分比设置内边距 */
-  border-radius: 8px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+.container-left{
+  width: 60%;
+  border: 1px solid yellow;
+  margin: 10px;
+}
+.middle{
+  margin: 10px;
+  display: flex;
 }
 
-h1 {
-  margin-bottom: 5vh; /* 设置下边距为视口高度的百分比 */
+.region{
+  height: 32px;
+  margin: 10px;
 }
 
-.post-form {
-  padding: 5vw; /* 以视口宽度的百分比设置内边距 */
-  background: #f5f5f5;
-  border-radius: 8px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+.el-tag{
+  margin: 10px;
+  height: 40px;
+  //text-align: center;
+  //vertical-align: center;
 }
 
-.form-group {
-  margin-bottom: 20px;
+.el-tag + .el-tag {
+  margin-left: 10px;
+}
+.button-new-tag {
+  margin: 10px;
+
+  height: 40px;
+  line-height: 40px;
+  padding-top: 0;
+  padding-bottom: 0;
+}
+.input-new-tag {
+  margin: 10px;
+  width: 90px;
+  vertical-align: center;
 }
 
-label {
-  display: block;
-  font-weight: bold;
-  margin-bottom: 5px;
-}
-
-input, textarea {
-  width: 100%;
-  padding: 8px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  box-sizing: border-box;
-}
-
-textarea {
-  height: 150px;
+.el-textarea__inner{
   resize: none;
 }
 
-button {
-  display: block;
-  margin-top: 15px;
-  padding: 10px 20px;
-  background-color: #0099ff;
-  color: white;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-  font-size: 16px;
+.aside{
+  border: 1px solid yellow;
+  margin: 10px;
 }
+
 </style>
